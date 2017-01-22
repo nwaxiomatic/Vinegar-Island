d05c2924f09a54a9c90f5a9726a3f55cf30b87f8
mac <nicholasdwoodward@gmail.com>
2017-01-22 17:43:02 -0500

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,28 @@
+var supportsAutoplay = false;
+var waitTime;
+
+$('#bg-video').vide({
+	mp4: 'static/media/video/bg.mp4',
+	poster: 'static/media/img/bg.jpg',
+}, {
+	posterType: 'jpg',
+	origin: 'content-box',
+});
+instance = $('#bg-video').data('vide');
+video = instance.getVideoObject();
+video.addEventListener('play', function () {
+    supportsAutoplay = true;
+});
+setTimeout(          //wait for listener to run
+    function(){
+        if(supportsAutoplay){
+        	video.onloadeddata = function(){
+	$('#bg-video-box').addClass('shown');
+}
+        }
+        else {
+        	$('#bg-video-box').addClass('shown');
+        }
+    },
+    waitTime
+); 
